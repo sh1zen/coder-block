705bd8d37d44abdc239b260a2e8f8c917585043e
sh1zen <dev.sh1zen@outlook.it>
2022-11-17 00:37:18 +0100

    1.0.4

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'eedfb2ce00f48f571a9e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7b8ef9e6407c2b31142b');
--- coder-block.php
+++ coder-block.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Coder Block
  * Plugin URI:        https://github.com/sh1zen/coder-block
  * Description:       A simple way to share and valuate code within the new Block Editor.
- * Version:           1.0.3
+ * Version:           1.0.4
  * Author:            sh1zen
  * Author URI:        https://sh1zen.github.io/
  * Text Domain:       coder-block
